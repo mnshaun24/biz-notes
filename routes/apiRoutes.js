261adf77482244376299cf4cb9e92e10583ba3ea
Shaun S <shaun.sticka@outlook.com>
2021-12-16 20:56:49 -0600

    pushed for help not much changed since last commit

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+// require files
+const router = require("express").Router();
+const { notes } = require("../db/db.json");
+
+router.get("/api/notes", (req, res) => {
+    let notes = 
+})
+
+
+
+
+// export for use
+module.exports = router;
--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+
+// standard codes
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
 // create routes
 app.use("/", htmlRoutes);
-
-app.use(express.static("public"));
 
 
 // set up the port
